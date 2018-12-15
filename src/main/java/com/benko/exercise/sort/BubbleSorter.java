904f7c07b03d590f914f9429337899360bf00316
robertbenko <robert.benko@gmail.com>
2018-12-15 15:57:11 +0100

    sort & printNumbers moved to SortingApp, access modifiers changed, for-loop changed to for-each, renaming parameters

--- src/main/java/com/benko/exercise/sort/BubbleSorter.java
+++ src/main/java/com/benko/exercise/sort/BubbleSorter.java
@@ -1,31 +1,15 @@
 package com.benko.exercise.sort;
 
-public class BubbleSorter {
+class BubbleSorter {
 
-    public void sort(int[] numbers) {
-        for (int i = 0; i < numbers.length; i++) {
-            swapElements(numbers);
-        }
-    }
-
-    private static void swapElements(int[] numbers) {
-        for (int i = 0; i < numbers.length-1; i++) {
-            if (isGreaterThan(i, i+1, numbers)) {
+    void swapElements(int actual, int[] numbers) {
                 int temp;
-                temp = numbers[i + 1];
-                numbers[i + 1] = numbers[i];
-                numbers[i] = temp;
-            }
-        }
+                temp = numbers[actual + 1];
+                numbers[actual + 1] = numbers[actual];
+                numbers[actual] = temp;
     }
 
-    private static boolean isGreaterThan(int x, int y, int[] numbers) {
-        return numbers[x]>numbers[y];
-    }
-
-    public static void printNumbers(int[] sortedNumbers) {
-        for (int i = 0; i < sortedNumbers.length; i++) {
-            System.out.println(sortedNumbers[i]);
-        }
+    boolean isGreaterThan(int current, int next, int[] numbers) {
+        return numbers[current] > numbers[next];
     }
 }
--- src/main/java/com/benko/exercise/sort/SortingApp.java
+++ src/main/java/com/benko/exercise/sort/SortingApp.java
@@ -1,11 +1,27 @@
 package com.benko.exercise.sort;
 
 public class SortingApp {
-    public static void main(String[] args) {
 
-        BubbleSorter BubbleSorter = new BubbleSorter();
+    public static void main(String[] args) {
         int[] numbers = new int[]{4, 2, 0, 2, 10, 1, 9, 12, 3, 5};
-        BubbleSorter.sort(numbers);
-        BubbleSorter.printNumbers(numbers);
+        SortingApp.sort(numbers);
+    }
+
+    private static void sort(int[] numbers) {
+        BubbleSorter bubbleSorter = new BubbleSorter();
+        for (int x: numbers) {
+            for (int i = 0; i < numbers.length - 1; i++){
+                if (bubbleSorter.isGreaterThan(i, i+1, numbers)) {
+                    bubbleSorter.swapElements(i, numbers);
+                }
+            }
+        }
+        SortingApp.printNumbers(numbers);
+    }
+
+    private static void printNumbers(int[] sortedNumbers) {
+        for (int x: sortedNumbers){
+            System.out.println(x);
+        }
     }
 }
