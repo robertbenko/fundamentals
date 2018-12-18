90e76b6970c24d0e97e299c976d942c74614185b
robertbenko <robert.benko@gmail.com>
2018-12-18 21:21:29 +0100

    sort method moved to BubbleSorter, syntax for-each loop corrected

--- src/main/java/com/benko/exercise/sort/BubbleSorter.java
+++ src/main/java/com/benko/exercise/sort/BubbleSorter.java
@@ -2,14 +2,28 @@ package com.benko.exercise.sort;
 
 class BubbleSorter {
 
-    void swapElements(int actual, int[] numbers) {
+    static void sort(int[] numbers) {
+        BubbleSorter bubbleSorter = new BubbleSorter();
+        for (int n: numbers) {
+            for (int i = 0; i < numbers.length - 1; i++){
+                if (bubbleSorter.isGreaterThan(i, i+1, numbers)) {
+                    bubbleSorter.swapElements(i, numbers);
+                }
+            }
+        }
+        SortingApp.printNumbers(numbers);
+    }
+
+    private boolean isGreaterThan(int current, int next, int[] numbers) {
+        return numbers[current] > numbers[next];
+    }
+
+    private void swapElements(int actual, int[] numbers) {
                 int temp;
                 temp = numbers[actual + 1];
                 numbers[actual + 1] = numbers[actual];
                 numbers[actual] = temp;
     }
 
-    boolean isGreaterThan(int current, int next, int[] numbers) {
-        return numbers[current] > numbers[next];
-    }
+
 }
--- src/main/java/com/benko/exercise/sort/SortingApp.java
+++ src/main/java/com/benko/exercise/sort/SortingApp.java
@@ -4,24 +4,12 @@ public class SortingApp {
 
     public static void main(String[] args) {
         int[] numbers = new int[]{4, 2, 0, 2, 10, 1, 9, 12, 3, 5};
-        SortingApp.sort(numbers);
+        BubbleSorter.sort(numbers);
     }
 
-    private static void sort(int[] numbers) {
-        BubbleSorter bubbleSorter = new BubbleSorter();
-        for (int x: numbers) {
-            for (int i = 0; i < numbers.length - 1; i++){
-                if (bubbleSorter.isGreaterThan(i, i+1, numbers)) {
-                    bubbleSorter.swapElements(i, numbers);
-                }
-            }
-        }
-        SortingApp.printNumbers(numbers);
-    }
-
-    private static void printNumbers(int[] sortedNumbers) {
-        for (int x: sortedNumbers){
-            System.out.println(x);
+    static void printNumbers(int[] sortedNumbers) {
+        for (int s: sortedNumbers){
+            System.out.println(s);
         }
     }
 }
