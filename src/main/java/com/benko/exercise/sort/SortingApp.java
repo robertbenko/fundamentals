afe799efd47c6c451f344faf2b11a48d40d0b942
robertbenko <robert.benko@gmail.com>
2019-01-06 12:43:33 +0100

    #3 invoking of sort method changed

--- src/main/java/com/benko/exercise/sort/SortingApp.java
+++ src/main/java/com/benko/exercise/sort/SortingApp.java
@@ -3,9 +3,9 @@ package com.benko.exercise.sort;
 public class SortingApp {
 
     public static void main(String[] args) {
-        Sorter bubbleSorter = new BubbleSorter();
+        Sorter sorter = new BubbleSorter();
         int[] numbers = new int[]{4, 2, 0, 2, 10, 1, 9, 12, 3, 5};
-        bubbleSorter.sort(numbers);
+        sorter.sort(numbers);
         printNumbers(numbers);
     }
 
