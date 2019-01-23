31b9efd2f057be951b71bb4a8bae43dbc790a4c7
robertbenko <robert.benko@gmail.com>
2019-01-23 13:31:32 +0100

    pom and comment changed

--- src/main/java/com/benko/exercise/sort/Sorter.java
+++ src/main/java/com/benko/exercise/sort/Sorter.java
@@ -1,8 +1,5 @@
 package com.benko.exercise.sort;
 
-/**
- * Created by Robert on 2019-01-06.
- */
 public interface Sorter {
     void sort(int[] numbers);
 }
--- src/main/java/com/benko/exercise/sort/SortingApp.java
+++ src/main/java/com/benko/exercise/sort/SortingApp.java
@@ -4,7 +4,9 @@ public class SortingApp {
 
     public static void main(String[] args) {
         Sorter sorter = new BubbleSorter();
+        //Sorter dummySorter = new DummySorter();
         int[] numbers = new int[]{4, 2, 0, 2, 10, 1, 9, 12, 3, 5};
+        //dummySorter.sort(numbers);
         sorter.sort(numbers);
         printNumbers(numbers);
     }
